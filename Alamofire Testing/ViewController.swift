d4e604972fc47f581db8e16ecbcb73cb76548a59
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-08-09 14:39:35 +0500

    Initial Commit

---
+++ Alamofire Testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alamofire Testing
+//
+//  Created by Hamza Rafique Azad on 9/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
