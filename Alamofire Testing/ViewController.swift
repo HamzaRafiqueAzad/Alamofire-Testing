ca86d654cedd7d67ab964f6ff44239559faec734
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-08-09 15:19:34 +0500

    Created button for executing json call and displayed it on the view.

--- Alamofire Testing/ViewController.swift
+++ Alamofire Testing/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
+    private let networkingClient = NetworkingClient()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func executeButton(_ sender: UIButton) {
+        guard let urlToExecute = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        networkingClient.execute(urlToExecute) { (json, error) in
+            if let error = error {
+                self.textView.text = error.localizedDescription
+            } else if let json = json {
+                self.textView.text = json.description
+            }
+        }
+    }
+    
 }
 
